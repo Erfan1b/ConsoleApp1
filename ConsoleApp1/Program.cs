c8188b131d2602dd728527d04999e7cbae801625
Erfan1b <erfanbahrami10@gmail.com>
2024-05-01 10:52:58 +0330

    added

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,9 +2,11 @@
 if((a%2)==0)
 {
     Console.WriteLine($"your input is an even number");
+    global::System.Console.WriteLine(a);
 }
 else if((a%2)==1)
 {
     Console.WriteLine($"your input is an odd number");
+    global::System.Console.WriteLine(a);
 }
 Console.ReadKey();
