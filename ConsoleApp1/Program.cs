103ec32e718a6ab416ff817cae4569ae382efba2
Erfan1b <erfanbahrami10@gmail.com>
2024-05-01 09:38:17 +0330

    the code now can detect odd or even numbers

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,10 @@
 ﻿int a = int.Parse(Console.ReadLine());
-Console.WriteLine(a);
+if((a%2)==0)
+{
+    Console.WriteLine($"your input is an even number");
+}
+else if((a%2)==1)
+{
+    Console.WriteLine($"your input is an odd number");
+}
+Console.ReadKey();
