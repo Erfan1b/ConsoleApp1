9eeffbfd19d44ead8b007e6c54cd4f661417fab9
Erfan1b <erfanbahrami10@gmail.com>
2024-05-01 11:13:12 +0330

    ,l

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,4 +9,8 @@ else if((a%2)==1)
     Console.WriteLine($"your input is an odd number");
     global::System.Console.WriteLine(a);
 }
+else
+{
+    global::System.Console.WriteLine("Error code 101");
+}
 Console.ReadKey();
